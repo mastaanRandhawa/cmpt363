69ce8511972a753aab5ab58b90ae36a0ad406403
Tasha Gandevia <100049187+TashaGandevia@users.noreply.github.com>
2026-04-01 13:57:41 -0700

    includes instructions

--- src/data/taskBreakdown.js
+++ src/data/taskBreakdown.js
@@ -4,12 +4,12 @@
 import useAIStore from './useAIStore'
 import { api } from './api'
 
-export async function generateSubtasks(task) {
+export async function generateSubtasks(task, instructions) {
     const { setLoading, setSuccess, setError } = useAIStore.getState()
     setLoading(`breakdown: ${task.name}`)
 
     try {
-        const { subtasks } = await api.breakdown(task)
+        const { subtasks } = await api.breakdown(task, instructions)
         setSuccess(JSON.stringify(subtasks, null, 2), subtasks)
         return subtasks
     } catch (err) {
